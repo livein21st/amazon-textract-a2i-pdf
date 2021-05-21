abacc112098175897c366fe9a20ab447b83de4ae
livein21st <niravpmalsattar@gmail.com>
2021-05-21 22:35:02 +0200

    fix config file

--- multipagepdfa2i/multipagepdfa2i_stack.py
+++ multipagepdfa2i/multipagepdfa2i_stack.py
@@ -20,12 +20,7 @@
 # -------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------
 # ~ ENTER SAGEMAKER AUGMENTED AI WORKFLOW ARN HERE:
-SAGEMAKER_WORKFLOW_AUGMENTED_AI_ARN_EV = ""
-
-# -------------------------------------------------------------------------------------------
-# -------------------------------------------------------------------------------------------
-# -------------------------------------------------------------------------------------------
-
+import config as cfg
 from aws_cdk import (
     core,
     aws_s3,
@@ -41,6 +36,12 @@ from aws_cdk import (
     aws_events,
     aws_events_targets
 )
+SAGEMAKER_WORKFLOW_AUGMENTED_AI_ARN_EV = cfg.credentials["hr-workflow-arn"]
+
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+
 
 class Multipagepdfa2IStack(core.Stack):
 
@@ -48,42 +49,42 @@ class Multipagepdfa2IStack(core.Stack):
 
         task_pngextract = aws_stepfunctions_tasks.LambdaInvoke(
             self, "PDF. Conver to PNGs",
-            lambda_function = services["lambda"]["pngextract"],
+            lambda_function=services["lambda"]["pngextract"],
             payload_response_only=True,
-            result_path = "$.image_keys"
+            result_path="$.image_keys"
         )
 
         task_wrapup = aws_stepfunctions_tasks.LambdaInvoke(
             self, "Wrapup and Clean",
-            lambda_function = services["lambda"]["wrapup"]
+            lambda_function=services["lambda"]["wrapup"]
         )
 
         iterate_sqs_to_textract = aws_stepfunctions_tasks.SqsSendMessage(
             self, "Perform Textract and A2I",
-            queue=services["textract_sqs"], 
-            message_body = aws_stepfunctions.TaskInput.from_object({
+            queue=services["textract_sqs"],
+            message_body=aws_stepfunctions.TaskInput.from_object({
                 "token": aws_stepfunctions.Context.task_token,
                 "id.$": "$.id",
                 "bucket.$": "$.bucket",
                 "key.$": "$.key",
                 "wip_key.$": "$.wip_key"
             }),
-            delay= None,
+            delay=None,
             integration_pattern=aws_stepfunctions.ServiceIntegrationPattern.WAIT_FOR_TASK_TOKEN
         )
 
         process_map = aws_stepfunctions.Map(
             self, "Process_Map",
-            items_path = "$.image_keys",
+            items_path="$.image_keys",
             result_path="DISCARD",
-            parameters = {
+            parameters={
                 "id.$": "$.id",
                 "bucket.$": "$.bucket",
                 "key.$": "$.key",
                 "wip_key.$": "$$.Map.Item.Value"
             }
         ).iterator(iterate_sqs_to_textract)
-        
+
         choice_pass = aws_stepfunctions.Pass(
             self,
             "Image. Passing.",
@@ -92,15 +93,18 @@ class Multipagepdfa2IStack(core.Stack):
         )
 
         pdf_or_image_choice = aws_stepfunctions.Choice(self, "PDF or Image?")
-        pdf_or_image_choice.when(aws_stepfunctions.Condition.string_equals("$.extension", "pdf"), task_pngextract)
-        pdf_or_image_choice.when(aws_stepfunctions.Condition.string_equals("$.extension", "png"), choice_pass)
-        pdf_or_image_choice.when(aws_stepfunctions.Condition.string_equals("$.extension", "jpg"), choice_pass)
+        pdf_or_image_choice.when(aws_stepfunctions.Condition.string_equals(
+            "$.extension", "pdf"), task_pngextract)
+        pdf_or_image_choice.when(aws_stepfunctions.Condition.string_equals(
+            "$.extension", "png"), choice_pass)
+        pdf_or_image_choice.when(aws_stepfunctions.Condition.string_equals(
+            "$.extension", "jpg"), choice_pass)
 
         # Creates the Step Functions
         multipagepdfa2i_sf = aws_stepfunctions.StateMachine(
-            scope = self, 
-            id = "multipagepdfa2i_stepfunction",
-            state_machine_name = "multipagepdfa2i_stepfunction",
+            scope=self,
+            id="multipagepdfa2i_stepfunction",
+            state_machine_name="multipagepdfa2i_stepfunction",
             definition=pdf_or_image_choice.afterwards().next(process_map).next(task_wrapup)
         )
 
@@ -109,7 +113,7 @@ class Multipagepdfa2IStack(core.Stack):
     def configure_dynamo_table(self, table_name, primary_key, sort_key):
         demo_table = aws_dynamodb.Table(
             self, table_name,
-            table_name= table_name,
+            table_name=table_name,
             partition_key=aws_dynamodb.Attribute(
                 name=primary_key,
                 type=aws_dynamodb.AttributeType.STRING
@@ -123,8 +127,9 @@ class Multipagepdfa2IStack(core.Stack):
 
     def create_iam_role_for_lambdas(self):
         lam_roles = {}
-        
-        names = ["kickoff", "pngextract", "analyzepdf", "humancomplete", "wrapup"]
+
+        names = ["kickoff", "pngextract",
+                 "analyzepdf", "humancomplete", "wrapup"]
 
         for name in names:
             lam_roles[name] = aws_iam.Role(
@@ -247,12 +252,13 @@ class Multipagepdfa2IStack(core.Stack):
 
     def create_lambda_functions(self, services):
         lambda_functions = {}
-        
+
         lambda_functions["pngextract"] = aws_lambda.Function(
             scope=self,
             id="multipagepdfa2i_pngextract",
             function_name="multipagepdfa2i_pngextract",
-            code=aws_lambda.Code.from_asset("./deploy_code/multipagepdfa2i_pngextract/multipagepdfa2i_pngextract.jar"),
+            code=aws_lambda.Code.from_asset(
+                "./deploy_code/multipagepdfa2i_pngextract/multipagepdfa2i_pngextract.jar"),
             handler="Lambda::handleRequest",
             runtime=aws_lambda.Runtime.JAVA_11,
             timeout=core.Duration.minutes(15),
@@ -261,29 +267,31 @@ class Multipagepdfa2IStack(core.Stack):
         )
 
         lambda_functions["analyzepdf"] = aws_lambda.Function(
-                scope=self,
-                id="multipagepdfa2i_analyzepdf",
-                function_name="multipagepdfa2i_analyzepdf",
-                code=aws_lambda.Code.from_asset("./deploy_code/multipagepdfa2i_analyzepdf/"),
-                handler="lambda_function.lambda_handler",
-                runtime=aws_lambda.Runtime.PYTHON_3_8,
-                timeout=core.Duration.minutes(3),
-                memory_size=3000,
-                role=services["lam_roles"]["analyzepdf"],
-                environment= {
+            scope=self,
+            id="multipagepdfa2i_analyzepdf",
+            function_name="multipagepdfa2i_analyzepdf",
+            code=aws_lambda.Code.from_asset(
+                "./deploy_code/multipagepdfa2i_analyzepdf/"),
+            handler="lambda_function.lambda_handler",
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            timeout=core.Duration.minutes(3),
+            memory_size=3000,
+            role=services["lam_roles"]["analyzepdf"],
+            environment={
                     "sqs_url": services["textract_sqs"].queue_url,
                     "human_workflow_arn": SAGEMAKER_WORKFLOW_AUGMENTED_AI_ARN_EV
-                }
+            }
         )
 
-        names = [ "humancomplete", "wrapup" ]
+        names = ["humancomplete", "wrapup"]
 
         for name in names:
             lambda_functions[name] = aws_lambda.Function(
                 scope=self,
                 id="multipagepdfa2i_" + name,
                 function_name="multipagepdfa2i_" + name,
-                code=aws_lambda.Code.from_asset("./deploy_code/multipagepdfa2i_" + name + "/"),
+                code=aws_lambda.Code.from_asset(
+                    "./deploy_code/multipagepdfa2i_" + name + "/"),
                 handler="lambda_function.lambda_handler",
                 runtime=aws_lambda.Runtime.PYTHON_3_8,
                 timeout=core.Duration.minutes(15),
@@ -302,56 +310,59 @@ class Multipagepdfa2IStack(core.Stack):
         ]
         for extension in extensions:
             services["main_s3_bucket"].add_event_notification(
-                aws_s3.EventType.OBJECT_CREATED,  
+                aws_s3.EventType.OBJECT_CREATED,
                 aws_s3_notifications.SqsDestination(services["sf_sqs"]),
-                aws_s3.NotificationKeyFilter(prefix="uploads/", suffix=extension)
-            )    
-        
+                aws_s3.NotificationKeyFilter(
+                    prefix="uploads/", suffix=extension)
+            )
+
         services["lambda"]["kickoff"].add_event_source(
             aws_lambda_event_sources.SqsEventSource(
-                services["sf_sqs"], 
-                batch_size=1
-            )
-        )
-        
-        services["lambda"]["analyzepdf"].add_event_source(
-            aws_lambda_event_sources.SqsEventSource(
-                services["textract_sqs"], 
+                services["sf_sqs"],
                 batch_size=1
             )
         )
 
-        human_complete_target = aws_events_targets.LambdaFunction(services["lambda"]["humancomplete"])
+        services["lambda"]["analyzepdf"].add_event_source(
+            aws_lambda_event_sources.SqsEventSource(
+                services["textract_sqs"],
+                batch_size=1
+            )
+        )
+
+        human_complete_target = aws_events_targets.LambdaFunction(
+            services["lambda"]["humancomplete"])
 
         human_review_event_pattern = aws_events.EventPattern(
             source=["aws.sagemaker"],
             detail_type=["SageMaker A2I HumanLoop Status Change"]
         )
 
-        aws_events.Rule(self, 
-            "multipadepdfa2i_HumanReviewComplete", 
-            event_pattern=human_review_event_pattern,
-            targets=[human_complete_target]
-        )
+        aws_events.Rule(self,
+                        "multipadepdfa2i_HumanReviewComplete",
+                        event_pattern=human_review_event_pattern,
+                        targets=[human_complete_target]
+                        )
 
     def create_services(self):
         services = {}
         # S3 bucket
-        services["main_s3_bucket"] = aws_s3.Bucket(self, "multipagepdfa2i", removal_policy=core.RemovalPolicy.DESTROY)
-        self.configure_dynamo_table("multia2ipdf_callback", "jobid", "callback_token")
+        services["main_s3_bucket"] = aws_s3.Bucket(
+            self, "multipagepdfa2i", removal_policy=core.RemovalPolicy.DESTROY)
+        self.configure_dynamo_table(
+            "multia2ipdf_callback", "jobid", "callback_token")
 
         services["sf_sqs"] = aws_sqs.Queue(
             self, "multipagepdfa2i_sf_sqs",
-            queue_name = "multipagepdfa2i_sf_sqs",
+            queue_name="multipagepdfa2i_sf_sqs",
             visibility_timeout=core.Duration.minutes(5)
         )
 
         services["textract_sqs"] = aws_sqs.Queue(
             self, "multipagepdfa2i_textract_sqs",
-            queue_name = "multipagepdfa2i_textract_sqs",
+            queue_name="multipagepdfa2i_textract_sqs",
             visibility_timeout=core.Duration.minutes(3)
         )
-        
 
         services["lam_roles"] = self.create_iam_role_for_lambdas()
         services["lambda"] = self.create_lambda_functions(services)
@@ -360,28 +371,26 @@ class Multipagepdfa2IStack(core.Stack):
 
         # need to creak kick off here so we can pass the state machine arn...
         services["lambda"]["kickoff"] = aws_lambda.Function(
-                scope=self,
-                id="multipagepdfa2i_kickoff",
-                function_name="multipagepdfa2i_kickoff",
-                code=aws_lambda.Code.from_asset("./deploy_code/multipagepdfa2i_kickoff/"),
-                handler="lambda_function.lambda_handler",
-                runtime=aws_lambda.Runtime.PYTHON_3_8,
-                timeout=core.Duration.minutes(5),
-                memory_size=3000,
-                role=services["lam_roles"]["kickoff"],
-                environment= {
+            scope=self,
+            id="multipagepdfa2i_kickoff",
+            function_name="multipagepdfa2i_kickoff",
+            code=aws_lambda.Code.from_asset(
+                "./deploy_code/multipagepdfa2i_kickoff/"),
+            handler="lambda_function.lambda_handler",
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            timeout=core.Duration.minutes(5),
+            memory_size=3000,
+            role=services["lam_roles"]["kickoff"],
+            environment={
                     "sqs_url": services["sf_sqs"].queue_url,
                     "state_machine_arn": services["sf"].state_machine_arn
-                }
+            }
         )
 
         return services
-
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         services = self.create_services()
         self.create_events(services)
-
-        
